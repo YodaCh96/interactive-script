72b15584f1045c7ce30f92eb1c5b28ca1329a2f2
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2021-03-14 20:38:49 +0100

    Create Pirate.cs

---
+++ tutorial-01/Pirate.cs
@@ -0,0 +1,20 @@
+namespace InteractiveScript
+{
+    // Mit dem Schlüsselwort "class" kann eine neue Klasse definiert werden.
+    // Namen von Klassen werden üblicherweise mit einem Grossbuchstaben geschrieben.
+    class Pirate
+    {
+        // Zwischen den Klammern { und } werden die "Member" einer Klasse aufgelistet.
+
+        // Eine "Eigenschaft" besteht aus:
+        //  - Zugriffsmodifizierer (public)
+        //  - Datentyp (string)
+        //  - Name der Eigenschaft (name)
+        public string name;
+
+        public int age;
+
+        public Hat hat;
+    }
+
+}
